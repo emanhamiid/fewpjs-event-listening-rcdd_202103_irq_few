a6ec61bba477caf96d108dda98bf2a742e787e67
eman hamid <emanhharki@gmail.com>
2021-04-06 13:46:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let label = document.getElementByTagName("label");
 
 function addingEventListener() {
-           label.addEventListener();
+           addEventListener();
 }
 addingEventListener();
